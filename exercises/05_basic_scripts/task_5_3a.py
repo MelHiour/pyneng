259707246201f0de4e407255dfdd156eda20d031
melhiour <melhiour@gmail.com>
2018-12-01 23:42:14 +0300

    5.3a done

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -21,3 +21,19 @@ trunk_template = [
     'switchport trunk encapsulation dot1q', 'switchport mode trunk',
     'switchport trunk allowed vlan {}'
 ]
+
+questions = {'access': 'Enter VLAN number: ', 'trunk': 'Enter allowed VLAN list: '}
+
+# Input from user
+int_type = input('Interface type: ')
+int_number = input('Interface number: ')
+
+vlans = input(questions[int_type])
+
+# Set int_type equal to trunk_template or access_template via locals(). Basically, we use user prompt as part of varialbe name. Locals contains all definet variables.
+int_type = locals()[int_type+'_template']
+
+print('interface {}'.format(int_number))
+print('\n'.join(int_type).format(vlans))
+
+
