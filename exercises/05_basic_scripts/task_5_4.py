4630f55da475b9e31e5039f3133d8860d312f7e9
melhiour <melhiour@gmail.com>
2018-12-02 00:10:44 +0300

    5.4 done

--- exercises/05_basic_scripts/task_5_4.py
+++ exercises/05_basic_scripts/task_5_4.py
@@ -17,3 +17,9 @@ num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
 word_list = [
     'python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl'
 ]
+
+number = int(input('Specify the number: '))
+word = input('Specify the word: ')
+
+print(len(num_list) - num_list[::-1].index(number) - 1)
+print(len(word_list) - word_list[::-1].index(word) - 1)
