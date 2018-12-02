3f7d363dfff98ae68c6701928b8f1612cf81bf11
melhiour <melhiour@gmail.com>
2018-12-02 22:25:16 +0300

    6.1 Done

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -19,3 +19,24 @@ D: 224-239
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+ip_address = input('Please set the IP address: ')
+
+class_a = (1, 127)
+class_b = (128, 191)
+class_c = (192, 223)
+class_d = (224, 239)
+
+if int(ip_address[0]) in class_a \
+or int(ip_address[0]) in class_b \
+or int(ip_address[0]) in class_c:
+    print('IP address {} belongs to Unicast'.format(ip_address))
+elif int(ip_address[0]) in class_d:
+    print('IP address {} belongs to Multicast'.format(ip_address))
+elif ip_address == '255.255.255.255':
+    print('IP address {} is Local broadcast'.format(ip_address))
+elif ip_address == '0.0.0.0':
+    print('IP address {} is Local unassigned'.format(ip_address))
+else:
+    print('IP address {} is unused'.format(ip_address))
+
