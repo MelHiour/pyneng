c73ae556dc7b732d56b186beddafcae539be39e2
melhiour <melhiour@gmail.com>
2018-12-07 12:27:33 +0300

    7.2a Done

--- exercises/07_files/task_7_2a.py
+++ exercises/07_files/task_7_2a.py
@@ -13,3 +13,16 @@
 '''
 
 ignore = ['duplex', 'alias', 'Current configuration']
+
+from sys import argv
+
+with open(argv[1]) as config:
+    file = config.read().split('\n')
+    for line in file:
+        if not line.startswith('!'):
+            for word in ignore:
+                if word in line:
+                    break
+                elif len(ignore)-1 == ignore.index(word):
+                    print(line)
+
