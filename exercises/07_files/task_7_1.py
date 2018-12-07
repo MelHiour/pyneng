d5dcc143635bc2aebb8815a1fa27e383804040f7
melhiour <melhiour@gmail.com>
2018-12-07 07:55:33 +0300

    7.1 Done

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,28 @@ Outbound Interface:    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+template = '''
+Protocol:\t {0[0]}
+Prefix:\t\t {0[1]}
+AD/Metric:\t {0[2]}
+Next-Hop:\t {0[4]}
+Last update:\t {0[5]}
+Interface:\t {0[6]}
+'''
+with open('ospf.txt') as file:
+    ospf_txt =  file.readlines()
+    for line in ospf_txt:
+        line_splitted = line.rstrip().split()
+        for item in line_splitted:
+             if item == 'O':
+                 line_splitted[line_splitted.index(item)] = 'OSPF'
+             else:
+                 line_splitted[line_splitted.index(item)] = line_splitted[line_splitted.index(item)].strip(',[]')
+
+        print(line)
+        print(template.format(line_splitted))
+
+
+                 
+
+
