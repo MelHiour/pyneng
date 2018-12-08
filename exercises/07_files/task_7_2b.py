e6fd67c0a64a9e433e9a2ad34adc781b8a545be8
melhiour <melhiour@gmail.com>
2018-12-08 15:32:15 +0300

    7.2c Done

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 '''
-Задание 7.2b
+Задание 7.2a
 
-Дополнить скрипт из задания 7.2a:
-* вместо вывода на стандартный поток вывода,
-  скрипт должен записать полученные строки в файл config_sw1_cleared.txt
+Сделать копию скрипта задания 7.2.
 
-При этом, должны быть отфильтрованы строки, которые содержатся в списке ignore.
-Строки, которые начинаются на '!' отфильтровывать не нужно.
+Дополнить скрипт:
+  Скрипт не должен выводить команды, в которых содержатся слова,
+  которые указаны в списке ignore.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
 
 ignore = ['duplex', 'alias', 'Current configuration']
+
+from sys import argv
+
+with open(argv[1]) as config:
+    file = config.read().split('\n')
+    for line in file:
+        if not line.startswith('!'):
+            for word in ignore:
+                if word in line:
+                    break
+                elif len(ignore)-1 == ignore.index(word):
+                    print(line)
+
