8f01d574718f6c5d02726e64f6d680c499256277
melhiour <melhiour@gmail.com>
2018-12-08 17:21:53 +0300

    7.3b Done

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -11,3 +11,13 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+vlan = input('Enter VLAN ID: ')
+
+with open('CAM_table.txt') as cam:
+    file = cam.readlines()
+    for line in file:
+        if '.' in line:
+            line_list = line.split()
+            line_list.remove('DYNAMIC')
+            if line_list[0] == vlan:
+                print("   ".join(line_list))
