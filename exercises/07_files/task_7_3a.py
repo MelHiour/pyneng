796dbb411bf771eb2aaff6a2abf1aa305fdecb33
melhiour <melhiour@gmail.com>
2018-12-08 17:32:46 +0300

    7.3a Done

--- exercises/07_files/task_7_3a.py
+++ exercises/07_files/task_7_3a.py
@@ -11,3 +11,15 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+with open('CAM_table.txt') as cam:
+    result_lol = []
+    for line in cam:
+        if '.' in line:
+            line_list = line.strip().split()
+            line_list.remove('DYNAMIC')
+            result_lol.append(line_list)
+    result_lol.sort()
+    for list in result_lol:
+        print('   '.join(list))
+
