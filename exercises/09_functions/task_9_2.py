5720993b1de3ac7da037e24011b088f34155abd6
melhiour <melhiour@gmail.com>
2018-12-10 08:06:50 +0300

    9.1b Done

--- exercises/09_functions/task_9_2.py
+++ exercises/09_functions/task_9_2.py
@@ -33,9 +33,22 @@ def generate_trunk_config(trunk):
         'switchport trunk native vlan 999', 'switchport trunk allowed vlan'
     ]
 
+    result = []
+
+    for intf, vlans in trunk.items():
+        result.append(intf)
+        for line in trunk_template:
+            if line.endswith('vlan'):
+                result.append('{} {}'.format(line,vlan))
+            else:
+                result.append(*vlans)
+    return result
+
 
 trunk_dict = {
     'FastEthernet0/1': [10, 20, 30],
     'FastEthernet0/2': [11, 30],
     'FastEthernet0/4': [17]
 }
+
+generate_trunk_config(trunk_dict)
