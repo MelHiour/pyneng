a5b9c794d0a8dfc2ae511daed26d73eeb78cdc32
melhiour <melhiour@gmail.com>
2018-12-22 16:45:42 +0300

    17.1 Done

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -44,8 +44,37 @@
 '''
 
 import glob
+import csv
+import re
 
 sh_version_files = glob.glob('sh_vers*')
-#print(sh_version_files)
+# print(sh_version_files)
 
 headers = ['hostname', 'ios', 'image', 'uptime']
+
+def parse_sh_version(output):
+    '''Parse "sh ver" and return tuple (software version, image, uptime)'''
+    regex = re.compile('Cisco IOS Software.+ Version (?P<ios>[\w.()]+)|file is "(?P<image>.+)"|uptime is (?P<uptime>.+)s')
+    iterator = regex.finditer(output)
+    result = tuple(item.group(item.lastgroup) for item in iterator)
+    result = tuple([result[0], result[2], result[1]]) # agly shit... please ignore...
+    return result
+
+def write_to_csv(to, what):
+    '''Write list of lists to .csv file'''
+    with open(to, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(what)
+
+##########################################################
+what = []
+what.append(headers)
+
+for file in sh_version_files:
+    hostname = re.search('\D\d', file).group()
+    with open(file) as sh_ver:
+        info = list(parse_sh_version(sh_ver.read()))    # Parse file and create a list from tuple
+        info.insert(0, hostname)                        # Insert hostname
+        what.append(info)                               # Append list to list of lists
+
+write_to_csv('test.csv', what)
