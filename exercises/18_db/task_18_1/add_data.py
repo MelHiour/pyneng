da093baf6be130c0f89b5fe9cc16f09077d4179c
melhiour <melhiour@gmail.com>
2018-12-24 16:29:33 +0300

    18.1 Done

--- exercises/18_db/task_18_1/add_data.py
+++ exercises/18_db/task_18_1/add_data.py
@@ -21,7 +21,63 @@ add_data.py
 '''
 
 import glob
+import sqlite3
+import re
+import yaml
 
 db_filename = 'dhcp_snooping.db'
 dhcp_snoop_files = glob.glob('sw*_dhcp_snooping.txt')
 #print(dhcp_snoop_files)
+
+def add_data(dhcp_list, db):
+    host_parse = re.compile('(.+?)_')
+    snoop_parse = re.compile('(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
+ 
+    result = []
+    
+    for file in dhcp_list:
+        hostname = host_parse.search(file).group(1)
+        with open(file) as shoop:
+            for line in shoop:
+                match = snoop_parse.search(line)
+                if match:
+                    result_list = []
+                    result_list = list(match.groups())
+                    result_list.append(hostname)
+                    result.append(tuple(result_list))
+    
+    conn = sqlite3.connect(db)
+    
+    for row in result:
+        try:
+            with conn:
+                query = '''insert into dhcp (mac, ip, vlan, interface, switch) values (?, ?, ?, ?, ?)'''
+                conn.execute(query, row)
+        except sqlite3.IntegrityError as e:
+            print('An error occured: ', e)
+    conn.close()
+
+def yaml_to_db(yaml_file, db):
+    to_db = []
+    with open(yaml_file) as file:
+        print(file)
+        result = yaml.load(file)
+        for value in result.values():
+            for key, value in value.items():
+                to_db.append(tuple([key, value]))
+
+    conn = sqlite3.connect(db)
+
+    for row in to_db:
+        try:
+            with conn:
+                query = '''insert into switches (hostname, location) values (?, ?)'''
+                conn.execute(query, row)
+        except sqlite3.IntegrityError as e:
+            print('An error occured: ', e)
+    conn.close()
+   
+
+
+add_data(dhcp_snoop_files, db_filename)
+yaml_to_db('switches.yml', db_filename)
--- exercises/18_db/task_18_1/create_db.py
+++ exercises/18_db/task_18_1/create_db.py
@@ -17,6 +17,25 @@ create_db.py
 Какие именно функции и как разделить код, надо решить самостоятельно.
 Часть кода может быть глобальной.
 """
+import os
+import sqlite3
 
 db_filename = 'dhcp_snooping.db'
 schema_filename = 'dhcp_snooping_schema.sql'
+
+def create_db(db, schema):
+    db_exists = os.path.exists(db)
+    conn = sqlite3.connect(db)
+
+    if not db_exists:
+        print('Creating schema...')
+        with open(schema) as file:
+            schema = file.read()
+        conn.executescript(schema)
+        print('DB has been created')
+    else:
+        print('Database exists')
+
+if __name__ == '__main__':
+    create_db(db_filename, schema_filename)
+
