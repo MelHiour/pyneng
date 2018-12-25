5094ee46e0d89a58dfa60c20717769ae74cc72d2
melhiour <melhiour@gmail.com>
2018-12-25 08:16:51 +0300

    18.2 Done

--- exercises/18_db/task_18_2/get_data.py
+++ exercises/18_db/task_18_2/get_data.py
@@ -67,3 +67,48 @@ switch      : sw2
 $ python get_data.py vlan
 Пожалуйста, введите два или ноль аргументов
 '''
+
+import sqlite3
+from tabulate import tabulate
+from sys import argv
+
+db_filename = 'dhcp_snooping.db'
+
+def get_full_dhcp():
+    conn = sqlite3.connect(db_filename)
+    result = []
+    for row in conn.execute('select * from dhcp'):
+        result.append(row)
+    print(tabulate(result, tablefmt='fancy_grid'))
+    conn.close()
+
+def get_key_value(key, value):
+    conn = sqlite3.connect(db_filename)
+    conn.row_factory = sqlite3.Row
+
+    query = 'select * from dhcp where {} = ?'.format(key)
+    result = conn.execute(query, (value, ))
+    for row in result:
+        print('-' * 40)
+        for k in keys:
+            print('{:12}: {}'.format(k, row[k]))
+    conn.close()
+
+if len(argv) == 1:
+    print('Return full talbe')
+    get_full_dhcp()
+elif len(argv) == 3:
+    key, value = argv[1:]
+    keys = ['mac', 'ip', 'vlan', 'interface', 'switch']
+    keys.remove(key)
+    print('Return infor for {} {}'.format(key, value))
+    get_key_value(key, value)
+else:
+    print('Error. Only two arguments are supported')
+
+
+
+
+
+
+
