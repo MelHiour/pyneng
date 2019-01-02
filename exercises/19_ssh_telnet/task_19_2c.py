b2b3f33eb01b1c50a38eeed26a390e9fd76f3f81
melhiour <melhiour@gmail.com>
2019-01-02 17:09:02 +0300

    19.2c Done

--- exercises/19_ssh_telnet/task_19_2c.py
+++ exercises/19_ssh_telnet/task_19_2c.py
@@ -1,25 +1,50 @@
 # -*- coding: utf-8 -*-
-'''
-Задание 19.2c
 
-Переделать функцию send_config_commands из задания 19.2b
+commands_with_errors = ['logging 0255.255.1', 'logging', 'i']
+correct_commands = ['logging buffered 20010', 'ip http server']
 
-Если при выполнении команды возникла ошибка,
-спросить пользователя надо ли выполнять остальные команды.
+commands = commands_with_errors + correct_commands
 
-Варианты ответа [y]/n:
-* y - выполнять остальные команды (значение по умолчанию)
-* n - не выполнять остальные команды
+import netmiko 
+import yaml
+from pprint import pprint
 
-Функция send_config_commands по-прежнему должна возвращать кортеж из двух словарей:
-* первый словарь с выводом команд, которые выполнились без ошибки
-* второй словарь с выводом команд, которые выполнились с ошибками
+with open('devices.yaml') as file:
+    devices = yaml.load(file)
+
+def send_config_command(device, config_commands, verbose = True):
+    result_correct = {}
+    result_incorrect = {}
+    try:
+        with netmiko.ConnectHandler(**device) as ssh:
+            print('Connecting to {}'.format(device['ip']))
+            ssh.enable()
+            for command in config_commands:
+                sent = ssh.send_config_set(command)
+                if 'Invalid input detected at' or 'Incomplete command' or 'Ambiguous command:' in sent:
+                    err_mess = input('There is an error. Continue [y]/n:')
+                    if err_mess == 'n':
+                        return 'There is an error'
+                        break
+                    else:
+                        result_incorrect[command] = sent
+                else:
+                    result_correct[command] = sent
+            if verbose:
+                result = ((result_correct),(result_incorrect))
+            else:
+                result = ((result_incorrect))
+            return result
+
+    except netmiko.NetMikoAuthenticationException:
+        return('Bad creds on {}'.format(device['ip']))
+
+    except netmiko.NetMikoTimeoutException:
+        return('Connection timeout to {}'.format(device['ip']))
 
 
-Оба словаря в формате
-* ключ - команда
-* значение - вывод с выполнением команд
-
-Проверить функцию на командах с ошибкой.
-
-'''
+if __name__ == '__main__':
+    for value in devices.values():
+        for device in value:
+            result = send_config_command(device, commands, verbose = True)
+            pprint(result)
