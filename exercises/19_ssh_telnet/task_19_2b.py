d4d6690aeeec7e8068e319e54b6cf2cc3d57fbbc
melhiour <melhiour@gmail.com>
2019-01-02 16:46:27 +0300

    19.2b Done

--- exercises/19_ssh_telnet/task_19_2b.py
+++ exercises/19_ssh_telnet/task_19_2b.py
@@ -42,3 +42,45 @@ commands_with_errors = ['logging 0255.255.1', 'logging', 'i']
 correct_commands = ['logging buffered 20010', 'ip http server']
 
 commands = commands_with_errors + correct_commands
+
+import netmiko 
+import yaml
+from pprint import pprint
+
+with open('devices.yaml') as file:
+    devices = yaml.load(file)
+
+def send_config_command(device, config_commands, verbose = True):
+    result_correct = {}
+    result_incorrect = {}
+    try:
+        with netmiko.ConnectHandler(**device) as ssh:
+            print('Connecting to {}'.format(device['ip']))
+            ssh.enable()
+            for command in config_commands:
+                sent = ssh.send_config_set(command)
+                if 'Invalid input detected at' in sent:
+                    result_incorrect[command] = sent
+                if 'Incomplete command' in sent:
+                    result_incorrect[command] = sent
+                if 'Ambiguous command:' in sent:
+                    result_incorrect[command] = sent
+                else:
+                    result_correct[command] = sent
+            if verbose:
+                result = ((result_correct),(result_incorrect))
+            else:
+                result = ((result_incorrect))
+            return result
+    except netmiko.NetMikoAuthenticationException:
+        return('Bad creds on {}'.format(device['ip']))
+
+    except netmiko.NetMikoTimeoutException:
+        return('Connection timeout to {}'.format(device['ip']))
+
+
+if __name__ == '__main__':
+    for value in devices.values():
+        for device in value:
+            result = send_config_command(device, commands, verbose = False)
+            pprint(result)
