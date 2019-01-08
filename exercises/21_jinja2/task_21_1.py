8f87c88b835d672e9273215970e0b4cc06d1bcc9
melhiour <melhiour@gmail.com>
2019-01-08 08:15:18 +0300

    21.1 Done

--- exercises/21_jinja2/task_21_1.py
+++ exercises/21_jinja2/task_21_1.py
@@ -20,15 +20,19 @@ import sys
 import os
 
 #$ python cfg_gen.py templates/for.txt data_files/for.yml
-TEMPLATE_DIR, template = os.path.split(sys.argv[1])
-VARS_FILE = sys.argv[2]
+def generate_cfg_from_template(template_path, data_path):
+    TEMPLATE_DIR, template_file = os.path.split(template_path)
 
-env = Environment(
+    env = Environment(
     loader=FileSystemLoader(TEMPLATE_DIR),
     trim_blocks=True,
     lstrip_blocks=True)
-template = env.get_template(template_file)
+    template = env.get_template(template_file)
 
-vars_dict = yaml.load(open(VARS_FILE))
+    vars_dict = yaml.load(open(data_path))
 
-print(template.render(vars_dict))
+    return(template.render(vars_dict))
+
+if __name__ == '__main__':
+    result = generate_cfg_from_template('templates/for.txt', 'data_files/for.yml')
+    print(result)
