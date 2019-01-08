e083d1a3db3db5212a9a1366f49360c4f246acbf
melhiour <melhiour@gmail.com>
2019-01-08 13:09:19 +0300

    21.1b Done

--- exercises/21_jinja2/task_21_1b.py
+++ exercises/21_jinja2/task_21_1b.py
@@ -42,3 +42,30 @@ data_dict = {
     'id': 3,
     'name': 'R3'
 }
+
+from jinja2 import Environment, FileSystemLoader
+import yaml
+import json
+import sys
+import os
+from pprint import pprint
+
+#$ python cfg_gen.py templates/for.txt data_files/for.yml
+def generate_cfg_from_template(template_path, yaml_file = False, json_file = False, py_dict = False,  trim = True, lstrip = True):
+    TEMPLATE_DIR, template_file = os.path.split(template_path)
+
+    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR), trim_blocks=trim, lstrip_blocks=lstrip)
+    template = env.get_template(template_file)
+    if yaml_file:
+        vars_dict = yaml.load(open(yaml_file))
+    elif json_file:
+        vars_dict = json.load(open(json_file))
+    elif py_dict:
+        vars_dict = py_dict
+    else:
+        return('Bad data source')
+    return(template.render(vars_dict))
+
+if __name__ == '__main__':
+    result = generate_cfg_from_template('templates/for.txt', py_dict = data_dict)
+    print(result)
